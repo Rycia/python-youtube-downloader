4e232a962397cfb161b53e1844750a94069ffb7c
Rycia Soulblight <43384589+Rycia@users.noreply.github.com>
2022-10-17 20:06:06 -0500

    Add files via upload

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,7 +1,5 @@
 from operator import truediv
 import os
-import subprocess
-import string
 from tkinter.tix import Tree
 from pytube import Playlist, YouTube
 
@@ -26,28 +24,30 @@ debug_mode = False
 # Run VS Code as Admin to allow the terminal to run as admin-- required for using ffmpeg
 # If ffmpeg still doesn't work, try to put the \bin\ffmpeg.exe executable next to the script.
 
-TEXT_BLUE = "\033[94m"
-TEXT_GREEN = "\033[92m"
-TEXT_YELLOW = "\033[93m"
-TEXT_RED = "\033[91m"
 
-TEXT_LIGHT_GRAY = "\033[0;37m"
-TEXT_LIGHT_RED = "\033[1;31m"
-TEXT_LIGHT_GREEN = "\033[1;32m"
-TEXT_LIGHT_BLUE = "\033[1;34m"
+class format:
+    blue = "\033[94m"
+    green = "\033[92m"
+    yellow = "\033[93m"
+    red = "\033[91m"
 
-TEXT_DARK_GRAY = "\033[1;30m"
+    light_gray = "\033[0;37m"
+    light_red = "\033[1;31m"
+    light_green = "\033[1;32m"
+    light_blue = "\033[1;34m"
 
-TEXT_BOLD = "\033[1m"
-TEXT_ITALIC = "\x1B[3m"
-TEXT_UNDERLINE = "\033[4m"
+    dark_gray = "\033[1;30m"
 
-TEXT_RESET = "\033[0m"  # ", TEXT_RESET, "
+    bold = "\033[1m"
+    italic = "\x1B[3m"
+    underline = "\033[4m"
+
+    reset = "\033[0m"
 
 
 def TEXT_BREAK():
-    print(TEXT_BOLD, TEXT_BLUE,
-          ">>> >>> >>> >>> >>> >>> >>> >>> >>> >>> >>> >>> >>> >>> >>>", TEXT_RESET)
+    print(format.bold, format.blue,
+          ">>> >>> >>> >>> >>> >>> >>> >>> >>> >>> >>> >>> >>> >>> >>>", format.reset)
 
 
 def run(pl):
@@ -61,7 +61,7 @@ def run(pl):
     # download each item in the list
     for i, l in enumerate(links):
         print(" ", str(i)+" of "+str(len(links)) +
-              TEXT_LIGHT_GRAY, TEXT_ITALIC, " ("+str(round(((i/len(links))*100), 2))+"%)", TEXT_RESET)
+              format.light_gray, format.italic, " ("+str(round(((i/len(links))*100), 2))+"%)", format.reset)
         yt = YouTube(l)  # Convert link to YouTube object
 
         # takes the best resolution stream for best possible audio result
@@ -69,8 +69,8 @@ def run(pl):
 
         # gets the filename of the first audio stream
         default_filename = music.default_filename
-        print(TEXT_GREEN, TEXT_BOLD, "Downloading  ",
-              TEXT_RESET, default_filename)
+        print(format.green, format.bold, "Downloading  ",
+              format.reset, default_filename)
 
         # downloads first audio stream
         music.download()
@@ -95,7 +95,7 @@ def run(pl):
             os.remove(default_filename)
 
     TEXT_BREAK()
-    print(TEXT_GREEN, TEXT_BOLD, "\nComplete!", TEXT_RESET)
+    print(format.green, format.bold, "\nComplete!", format.reset)
     TEXT_BREAK()
 
 
@@ -103,18 +103,18 @@ if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')  # Clear command line
 
     if remove_source_file == False:
-        print(TEXT_ITALIC, TEXT_YELLOW, "remove_source_file", TEXT_RESET, TEXT_YELLOW,
-              " is set to ", TEXT_BOLD, "false!", TEXT_RESET, TEXT_YELLOW, "\nAs a result, you will be prompted to answer \"yes\" or \"no\" to anything ffmpeg may ask of you, primarily the annoying overwriting of duplicate files!\nSet it to \"True\"  at the top of the code to ignore this warning and automatically shadow-answer \"yes\" to any prompts!", TEXT_RESET)
+        print(format.italic, format.yellow, "remove_source_file", format.reset, format.yellow,
+              " is set to ", format.bold, "false!", format.reset, format.yellow, "\nAs a result, you will be prompted to answer \"yes\" or \"no\" to anything ffmpeg may ask of you, primarily the annoying overwriting of duplicate files!\nSet it to \"True\"  at the top of the code to ignore this warning and automatically shadow-answer \"yes\" to any prompts!", format.reset)
         TEXT_BREAK()
 
     if debug_mode == True:
-        print(TEXT_ITALIC, TEXT_YELLOW, "debug_mode", TEXT_RESET, TEXT_YELLOW,
-              " is set to ", TEXT_BOLD, "true!", TEXT_RESET, TEXT_YELLOW, "\nAs a result, you will see all the output coming from ffmpeg!\nSet it to \"False\"  at the top of the code to ignore this warning and hide ffmpeg's output!", TEXT_RESET)
+        print(format.italic, format.yellow, "debug_mode", format.reset, format.yellow,
+              " is set to ", format.bold, "true!", format.reset, format.yellow, "\nAs a result, you will see all the output coming from ffmpeg!\nSet it to \"False\"  at the top of the code to ignore this warning and hide ffmpeg's output!", format.reset)
         TEXT_BREAK()
 
     TEXT_BREAK()
-    print("                         Made by Rycia\n                  ", TEXT_ITALIC, TEXT_DARK_GRAY,
-          "  github.com/Rycia\n          github.com/Rycia/python-youtube-downloader", TEXT_RESET)
+    print("                         Made by Rycia\n                  ", format.italic, format.dark_gray,
+          "  github.com/Rycia\n          github.com/Rycia/python-youtube-downloader", format.reset)
 
     TEXT_BREAK()
     url = input("Enter the URL of the YouTube playlist you wish to download: ")
@@ -127,12 +127,12 @@ if __name__ == "__main__":
         try:
             run(pl)
         except Exception as thrown_error:
-            print(TEXT_RED, "ERROR | Enter a valid URL!\n Playlists should start with", TEXT_ITALIC, TEXT_UNDERLINE,
-                  "https://youtube.com/playlist?list=", TEXT_RESET, TEXT_RED,
-                  "\n You can obtain these under \"Share\" and \"Copy\" on the playlist page, not the link of any video within the playlist!", TEXT_RESET)
+            print(format.red, "ERROR | Enter a valid URL!\n Playlists should start with", format.italic, format.underline,
+                  "https://youtube.com/playlist?list=", format.reset, format.red,
+                  "\n You can obtain these under \"Share\" and \"Copy\" on the playlist page, not the link of any video within the playlist!", format.reset)
             print(thrown_error)
             TEXT_BREAK()
             exit(1)
     else:
-        print(TEXT_RED, "ERROR | debug_mode is neither True nor False and is therefore invalid. Set it to one of the two!", TEXT_RESET)
+        print(format.red, "ERROR | debug_mode is neither True nor False and is therefore invalid. Set it to one of the two!", format.reset)
         exit(2)
